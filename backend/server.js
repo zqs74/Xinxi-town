476d48bd798782b88242a51aab344064872b204b
zqs74 <1603712839@qq.com>
2026-02-26 18:57:36 +0800

    Update CORS configuration for new domain

--- backend/server.js
+++ backend/server.js
@@ -24,7 +24,7 @@ const app = express();
 
 // 中间件
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:5174', 'http://localhost:3000', 'http://localhost:8080'],
+  origin: ['http://localhost:5173', 'http://localhost:5174', 'http://localhost:3000', 'http://localhost:8080', 'https://www.xinxi-town.me'],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
